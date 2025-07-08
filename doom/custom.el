84a79713371c7485c191ce3381bea6f746d4cfb0
nathantebbs <nathan.tebbs2004@gmail.com>
2025-07-08 10:48:00 -0700

    clean

--- doom/custom.el
+++ doom/custom.el
@@ -4,8 +4,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(codeium/metadata/api_key
-   "sk-ws-01-9tOAlY_XCQi-zLGGWN6YFGyt-qJ7rT7rpJdwMgto6KrUt1JZaP06ilGi80sHNv-5pMQ4hcNO8M-MGnYooMhj9f6_diOpXg"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
