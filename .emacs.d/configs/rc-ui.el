cdddd4711bcde535721c0865aac806325f1260dc
Nathan Tebbs <vetr0s.dev@gmail.com>
2026-08-30 13:31:58 -0700

    Stop highlighting programming lines

--- .emacs.d/configs/rc-ui.el
+++ .emacs.d/configs/rc-ui.el
@@ -61,7 +61,6 @@
 
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
 (add-hook 'text-mode-hook #'display-line-numbers-mode)
-(add-hook 'prog-mode-hook #'hl-line-mode)
 
 ;; show-paren-mode has been on by default since Emacs 28, so it is not here.
 (electric-pair-mode 1)
